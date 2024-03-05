008c9fd8443df64e50edb2f8771c9825aaf886f0
“Aleksandr <ross1990@mail.ru>
2024-03-05 15:17:50 +0300

    Add template

--- sport_news/views.py
+++ sport_news/views.py
@@ -4,14 +4,18 @@ from django.shortcuts import render
 from sport_news.models import Article
 
 
+# def news_view(request):
+#     articles = Article.objects.order_by('-update_date')
+#     str1 = f'<title>Новости</title>'
+#     for news in articles:
+#         str1 += f'<body>' \
+#                 f'<h3>{news.title}</h3>' \
+#                 f'<p>{news.content}</p>' \
+#                 f'<p>{news.create_date.strftime("%Y-%m-%d %H:%M:%S")}</p>' \
+#                 f'</body>'
+#     print(str1)
+#     return HttpResponse(str1)
+
 def news_view(request):
-    articles = Article.objects.all()
-    str1 = f'<title>Новости</title>'
-    for news in reversed(articles):
-        str1 += f'<body>' \
-                f'<h3>{news.title}</h3>' \
-                f'<p>{news.content}</p>' \
-                f'<p>{news.create_date.strftime("%Y-%m-%d %H:%M:%S")}</p>' \
-                f'</body>'
-    print(str1)
-    return HttpResponse(str1)
+     articles = Article.objects.order_by('-update_date')
+     return render(request, 'sport_news/news.html', {'articles': articles, 'title': 'Новости'})
