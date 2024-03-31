0c55b543bda0d570a19a4b4d9200d1e89b2ec0ce
“Aleksandr <ross1990@mail.ru>
2024-03-31 18:29:01 +0300

    add inc tag

--- sport_news/templatetags/poll_extras.py
+++ sport_news/templatetags/poll_extras.py
@@ -1,9 +1,24 @@
 import datetime
 from django import template
 
+from sport_news.models import Category
+
 register = template.Library()
 
 
 @register.simple_tag
 def current_time(format_string):
     return datetime.datetime.now().strftime(format_string)
+
+@register.simple_tag
+def category_tag():
+    return Category.objects.all()
+
+
+@register.inclusion_tag('sport_news/categories.html')
+def category_inc(category_id):
+    category = Category.objects.all()
+    return {
+        'category': category,
+        'category_id': category_id
+    }
--- sport_news/views.py
+++ sport_news/views.py
@@ -20,35 +20,32 @@ from sport_news.models import Article, Category
 def news_view(request):
     # articles = Article.objects.order_by('-update_date')
     page_number = int(request.GET.get("articles", 1))
-    category = Category.objects.all()
+    # category = Category.objects.all()
     paginator = Paginator(Article.objects.filter(is_published=True), 5)
     articles = paginator.get_page(page_number)
     return render(request, 'sport_news/news.html', {
         'articles': articles,
-        'title': '',
-        'category': category
+        'title': ''
     })
 
 
 def category_view(request, category_id):
     page_number = int(request.GET.get("articles", 1))
-    category = Category.objects.all()
+    # category = Category.objects.all()
     paginator = Paginator(Article.objects.filter(category_id=category_id, is_published=True), 5)
     articles = paginator.get_page(page_number)
     title_name = Category.objects.get(pk=category_id)
     return render(request, 'sport_news/news.html', {
         'articles': articles,
         'title': title_name.title,
-        'category': category,
         'category_id': category_id
     })
 
 
 def article_view(request, article_id):
-    category = Category.objects.all()
+    # category = Category.objects.all()
     article = Article.objects.get(id=article_id, is_published=True)
     return render(request, 'sport_news/article.html', {
         'title': article.title,
-        'article': article,
-        'category': category
+        'article': article
     })
